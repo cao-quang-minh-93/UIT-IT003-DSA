260daf0ffe630026a411121a2f200207cff2f7eb
9Minh <63891954+cao-quang-minh-93@users.noreply.github.com>
2023-10-06 01:00:58 +0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+/* FUNCTIONS */
+#define f(a,b,n) for(int a=b;a<=n;a++)
+#define fd(a,b,n) for(int a=b;a>=n;a--)
+#define pb push_back
+#define eb emplace_back
+
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////////////////////////
+/***************************************--DECLARE--***************************************/
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+/***************************************--PROCESS--***************************************/
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+/***************************************--MAIN--***************************************/
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    freopen("process.inp","r",stdin);
+    freopen("process.out","w",stdout);
+
+    return 0;
+}
+////////////////////////////////////////////////////////////////////////////////////////////
+/***************************************--QMIN--***************************************/
